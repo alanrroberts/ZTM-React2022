879359b3e1ca11e44278189e7b811a83adeae52e
Alan Roberts <alan@statistixl.com>
2022-06-12 14:14:34 +0100

    pre sass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const categories = [
+        {
+            "title": "Hats"
+        },
+        {
+            "title": "Jackets"
+        },
+        {
+            "title": "Sneakers"
+        },
+        {
+            "title": "Womens"
+        },
+        {
+            "title": "Mens"
+        },
+    ]
+
+    return (
+        <div className="categories-container">
+            {
+                categories.map((category) => {
+                    return (
+                        < div className="category-container">
+                            {/* <img src='' /> */}
+                            <div className="category-body-container">
+                                <h2>{category.title}</h2>
+                                <p>Shop Now</p>
+                            </div>
+                        </div>
+                    );
+                })
+            }
+        </div >
+    );
 }
 
 export default App;
